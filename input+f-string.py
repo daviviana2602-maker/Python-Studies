6e9d95a902b5a149a1e10b48ae67277a2609bb58
Davi Fonseca Viana <daviviana2602@gmail.com>
2025-12-12 11:22:36 -0300

    Add files via upload

---
+++ input+f-string.py
@@ -0,0 +1,7 @@
+nome = input('olá, qual é o seu nome?')
+print(f'olá, {nome}! prazer em te conhecer!')
+dia = input(f'em que dia você nasceu, {nome}?')
+mes = input('que mes você nasceu?')
+ano = input('em que ano você nasceu?')
+print(f'você nasceu no dia {dia} de {mes} de {ano}. Correto?')
+
