4aa1cd5009406a5006b2c6b4283adc93173adb9e
Davi Fonseca Viana <daviviana2602@gmail.com>
2025-12-14 21:36:07 -0300

    Delete is(verif).py

--- is(verif).py
+++
@@ -1,9 +0,0 @@
-p = input('digite algo: ')
-print(p.isnumeric())  #verifica se é número
-print(p.isnumeric())  #verifica se é número
-print(p.isalpha())    #verifica se é letra
-print(p.isalnum())    #verifica se é letra ou número
-print(p.isupper())    #verifica se está em maiúsculo
-print(p.islower())    #verifica se está em minúsculo
-print(p.isspace())    #verifica se é espaço
-print(p.istitle())    #verifica se está capitalizada
