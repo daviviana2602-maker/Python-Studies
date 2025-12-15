f0cd6d121f529ebb406b1755aeb987272830414d
Davi Fonseca Viana <daviviana2602@gmail.com>
2025-12-14 23:56:16 -0300

    Update If+Else+Repetição.py

--- If+Else+Repetição.py
+++ If+Else+Repetição.py
@@ -10,6 +10,20 @@ if lucro > 1500:
     print("bom resultado")
 else:
     print("resultado ruim")
+
+arroz = float(input('qual o valor do arroz?'))
+print(f'o preço do arroz é {arroz:.2f} reais')
+if arroz >= 20:
+    print('valor alto')
+else:
+    print('valor baixo')
+feijão = float(input('e qual é o valor do feijão?'))
+print(f'o preço do feijão é {feijão:.2f} reais')
+if feijão >=14:
+    print('valor alto')
+else:
+    print('valor baixo')
     
 for i in range(3):
+
         print("quem será o funcionário do mês?")
