83a3115135b9a57c39341974a344ab511d787e9c
Davi Viana <daviviana2602@gmail.com>
2026-01-17 23:58:36 -0300

    Delete input+f-string.py

--- input+f-string.py
+++
@@ -1,7 +0,0 @@
-nome = input('olá, qual é o seu nome?')
-print(f'olá, {nome}! prazer em te conhecer!')
-dia = input(f'em que dia você nasceu, {nome}?')
-mes = input('que mes você nasceu?')
-ano = input('em que ano você nasceu?')
-print(f'você nasceu no dia {dia} de {mes} de {ano}. Correto?')
-
