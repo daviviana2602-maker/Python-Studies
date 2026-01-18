401f4d27ea751757debf30582162fe5cb8e44716
Davi Viana <daviviana2602@gmail.com>
2026-01-17 23:57:24 -0300

    Delete Listas.py

--- Listas.py
+++
@@ -1,9 +0,0 @@
-print ("lista de produtos")
-listadeprodutos=['iphone', 'carro', 'casa']
-for produto in listadeprodutos:
-    print(produto)
-    
-print ("lista de preços")
-listadepreços=[1000, 1500, 2000]
-for preço in listadepreços:
-    print(preço)
